95957c5599f13d24bf1f8d1230973aabe0426e36
balatos <matej.balat@gmail.com>
2022-08-11 21:31:38 +0200

    Fix dto

---
+++ src/robots/dto/robot-get.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDefined, IsString, IsUUID } from 'class-validator';
+
+export class RobotGetDto {
+  @ApiProperty()
+  @IsUUID()
+  id: string;
+
+  @ApiProperty()
+  @IsDefined()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsDefined()
+  @IsUUID()
+  batteryId: string;
+
+  constructor({ id, name, batteryId }: any = {}) {
+    this.id = id;
+    this.name = name;
+    this.batteryId = batteryId;
+  }
+}
