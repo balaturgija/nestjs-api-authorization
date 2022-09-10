51ff3e2a0d1fafb617fe3c16f9aba98a8053f6bb
balatos <matej.balat@gmail.com>
2022-09-11 00:13:05 +0200

    Abort generic controller

--- src/batteries/batteries.controller.ts
+++ src/batteries/batteries.controller.ts
@@ -1,18 +1,4 @@
-import { Controller, Get, Inject } from '@nestjs/common';
-import { BatteriesService } from './batteries.service';
-import { BatteryGetDto } from './dto/battery-get.dto';
+import { Controller } from '@nestjs/common';
 
 @Controller('batteries')
-export class BatteriesController {
-  constructor(
-    @Inject(BatteriesService)
-    private readonly batteriesService: BatteriesService,
-  ) {}
-
-  @Get()
-  async findAllAsync(): Promise<BatteryGetDto[]> {
-    const batteries = await this.batteriesService.getAll();
-    const dtos = batteries.map((battery) => new BatteryGetDto(battery));
-    return dtos;
-  }
-}
+export class BatteriesController {}
