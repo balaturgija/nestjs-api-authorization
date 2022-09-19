37dfcdbc45c9af85919580682cb7feb89899cdf4
balatos <matej.balat@gmail.com>
2022-09-19 12:02:16 +0200

    Created getbyId, post and delete endpoints

--- src/batteries/batteries.controller.ts
+++ src/batteries/batteries.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, HttpStatus, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 import { Response } from 'express';
 import BaseController from '../base/base.controller';
 import { BatteriesService } from './batteries.service';
 import { BatteryCreateDto } from './dto/battery.create.dto';
 import { BatteryDto } from './dto/battery.dto';
+import { BatteryParamDto } from './dto/battery.param.dto';
 // import { ErrorHandler } from '../helpers/ErrorHandler';
 // import { SuccessHandler } from '../helpers/SuccessHandler';
 // import { handleSuccess } from '../middleware/handleSuccess';
@@ -13,6 +23,22 @@ import { BatteryDto } from './dto/battery.dto';
 export class BatteriesController extends BaseController {
   constructor(private readonly batteriesService: BatteriesService) {
     super();
+  }
+
+  @Get(':id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: BatteryDto,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Not found',
+  })
+  async getById(@Res() res: Response, @Param() params: BatteryParamDto) {
+    const result = await this.batteriesService.getByIdAsync(params.id);
+    if (result != null) return this.Ok(res, result);
+
+    return this.NotFound(res, 'Battery not found.');
   }
 
   @Post()
@@ -29,7 +55,31 @@ export class BatteriesController extends BaseController {
     @Body() batteryCreateDto: BatteryCreateDto,
   ): Promise<Response> {
     const result = await this.batteriesService.createAsync(batteryCreateDto);
-    if (result.success) return this.Created(res, result);
+    if (result.success) return this.Created(res, batteryCreateDto);
+
+    return this.Error(res, result.errors);
+  }
+
+  @Delete(':id')
+  @ApiResponse({
+    status: HttpStatus.OK,
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Not found',
+  })
+  @ApiResponse({
+    status: HttpStatus.INTERNAL_SERVER_ERROR,
+    description: 'Error on delete',
+  })
+  async delete(
+    @Res() res: Response,
+    @Param() params: BatteryParamDto,
+  ): Promise<Response> {
+    const resource = await this.batteriesService.getByIdAsync(params.id);
+    if (!resource) return this.NotFound(res, 'Battery not found.');
+    const result = await this.batteriesService.deleteAsync(params.id);
+    if (result.success) return this.Ok(res);
 
     return this.Error(res, result.errors);
   }
--- src/batteries/batteries.service.ts
+++ src/batteries/batteries.service.ts
@@ -3,6 +3,8 @@ import { BatteryEntity } from './entities/battery.entity';
 import { Provider } from '../constants';
 import { ActionResult } from '../helpers/ActionResult';
 import { BatteryCreate } from './interfaces/batteryCreate.interface';
+import { WhereOptions } from 'sequelize';
+import { Battery } from './interfaces/battery.interface';
 // import { Repository } from 'sequelize-typescript';
 // import { BaseService } from '../base/base.service';
 
@@ -16,10 +18,25 @@ export class BatteriesService {
     // super(batteryRepository);
   }
 
+  async getByIdAsync(id: string): Promise<Battery | null> {
+    return await this.batteryRepository.findByPk(id);
+  }
+
   async createAsync(createBatteryDto: BatteryCreate): Promise<ActionResult> {
     const result = new ActionResult();
     const createResult = await this.batteryRepository.create(createBatteryDto);
     if (!createResult) result.AddError('Battery not created.');
+
+    return result;
+  }
+
+  async deleteAsync(id: string): Promise<ActionResult> {
+    const result = new ActionResult();
+    const whereClause: WhereOptions = { where: { id: id } };
+    const deleteResult =
+      (await this.batteryRepository.destroy(whereClause)) > 0;
+
+    if (!deleteResult) result.AddError('Error on delete');
 
     return result;
   }
