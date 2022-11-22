2e8274cddf4e681b42c4d9b07fdf87e33c15ac09
balaturgija <matej.balat@gmail.com>
2022-11-22 15:41:26 +0100

    Create batteries exist pipe

--- src/batteries/pipes/battery-exists.pipe.ts
+++ src/batteries/pipes/battery-exists.pipe.ts
@@ -1,10 +1,10 @@
 import { Injectable, PipeTransform } from '@nestjs/common';
+import { CreateUserDto } from '../../users/dto/create-user.dto';
 import { BatteriesService } from '../batteries.service';
-import { UserCreateDto } from '../../users/dto/create-user.dto';
 import { BatteryNotExistsException } from '../exceptions/battery-not-exists.exception';
 
 @Injectable()
-export class BatteryExistsPipe implements PipeTransform<UserCreateDto> {
+export class BatteryExistsPipe implements PipeTransform<CreateUserDto> {
     constructor(private readonly batteryService: BatteriesService) {}
     async transform(value: any) {
         const exists = await this.batteryService.exists(value);
