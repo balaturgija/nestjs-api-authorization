140ee079a8b54e90c87738a1365b5af7fd12f870
balaturgija <matej.balat@gmail.com>
2022-11-24 18:24:47 +0100

    Create socket adapter

---
+++ src/socket-io-adapter.ts
@@ -0,0 +1,41 @@
+import { INestApplicationContext } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+import { ServerOptions, Server } from 'socket.io';
+import { createTokenMiddleware } from './auth/auth.socket.middleware';
+
+dotenv.config({
+    path: path.resolve(`.env`),
+});
+
+export class SocketIOAdapter extends IoAdapter {
+    constructor(
+        private app: INestApplicationContext,
+        private configService: ConfigService
+    ) {
+        super(app);
+    }
+
+    createIOServer(port: number, options?: ServerOptions) {
+        const cors = {
+            origin: [`http://${process.env.HOST}:${process.env.PORT}`],
+        };
+
+        console.log(`Socket server runs on ${cors.origin[0]}`);
+
+        const optionsWithCORS: ServerOptions = {
+            ...options,
+            cors,
+        };
+
+        const jwtService = this.app.get(JwtService);
+        const server: Server = super.createIOServer(port, optionsWithCORS);
+
+        server.of('bids').use(createTokenMiddleware(jwtService));
+
+        return server;
+    }
+}
