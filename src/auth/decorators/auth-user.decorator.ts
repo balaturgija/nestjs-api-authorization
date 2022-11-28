f2ff4ebc061eb73de04f0e24828c7e7bbb0d94a8
balaturgija <matej.balat@gmail.com>
2022-11-28 10:37:07 +0100

    Fix wallet in jwt token

--- src/auth/decorators/auth-user.decorator.ts
+++ src/auth/decorators/auth-user.decorator.ts
@@ -2,5 +2,6 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 export const AuthUser = createParamDecorator((_, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
+    request.user.wallet.amount = Number(request.user.wallet.amount);
     return request.user;
 });
